86c24d5f14997ef326e6fd21abc5b1d217838306
Habibur Rahman Sagor <hrsagor34@gmail.com>
2025-03-06 12:21:47 +0600

    Add files via upload

---
+++ Template Literals.js
@@ -0,0 +1,14 @@
+
+  let name = 'Ali';
+  let age = 25;
+  let city= 'Istanbul';
+  console.log (`Hello, My name is ${name}. I am ${age} years old and I live in ${city}.`);
+
+
+
+function introduce(name, age, city) {
+    return `Hello, my name is ${name}. I am ${age} years old and I live in ${city} `;
+  }
+  
+  console.log(introduce("Ali", 25, "Istanbul"));
+
